57939c1a26f4af4747960fb1706695ee7c38aa1b
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2025-01-03 12:27:42 -0500

    i GOT THAT DAWG IN ME

---
+++ create task/list.js
@@ -0,0 +1,55 @@
+const DUNDADUNDADUN = [
+  {
+    quote:
+      "this is so unhealthy that by the time you guys 13 your double chins will be touching your cankles",
+    season: "3",
+    episode: "28",
+  },
+  {
+    quote:
+      "oh mommy I want some chocolate. Im sorry little sausage but you've had too much candy this week. meh meh meh meh meh. *COMBUSTION* ",
+    season: "2",
+    episode: "30",
+  },
+  {
+    quote:
+      "darwin I can't believe you just did that! did what? I dont remember.",
+    season: "2",
+    episode: "21",
+  },
+  {
+    quote: "bobert terminate gumball or whatever",
+    season: "2",
+    episode: "50",
+  },
+  {
+    quote: "you can walk? yep! and unfortunately you can't anymore!",
+    season: "",
+    episode: "",
+  },
+  {
+    quote:
+      "I dont know what that secret ingredient is but the customers just love your burgers! The secret ingredient is love",
+    season: "",
+    episode: "",
+  },
+  {
+    quote: "I can't lose this job I only have 37 dollars!",
+    season: "5",
+    episode: "27",
+  },
+  {
+    quote: "79% of stair accidents happen on the stairs.",
+  },
+  {
+    quote: "Out of my way, my kids are in danger... of becoming their father!",
+  },
+  {
+    quote:
+      "I think we all know where this is going so lets just skip to the end. *BOOM*",
+  },
+  {
+    quote:
+      "Uh, if the food enters your body and exits it while it's still in the supermarket, it's technically not stealing.",
+  },
+];
