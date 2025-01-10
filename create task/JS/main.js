5d998fc16a35c95d92c9cb4734d985752ee3bea7
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2025-01-10 12:30:49 -0500

    xdh

--- create task/JS/main.js
+++ create task/JS/main.js
@@ -5,3 +5,17 @@
 // quote
 // box for season and episode
 // #/# questions correct
+
+
+const episode_input = 
+const season_episode = 
+
+//shuffle the array randomly and display it in order
+// make a function called curepisode/season that takes the episode and season of the current displayed quote
+//use a function that compares the inputs to the curepisode/season
+// if else stament for correct and incorrect
+//only penalty is that it pushes into next function
+//state whether wrong or correct, display season and episode
+//function that adds to the correct or incorrect #/# correct, adds 1 to 2nd, only adds to 1st if correcyt
+//
+
